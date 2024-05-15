f05b576772414a6a008008b979699feffb5f040f
Francis Iwu <apkfem@gmail.com>
2024-05-15 02:48:53 +0200

    bug fix

--- utils/index.js
+++ utils/index.js
@@ -2,7 +2,5 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-  "postgresql://spendwisedb_owner:Al9jdSQBC3sb@ep-autumn-cloud-a2cvgstr.eu-central-1.aws.neon.tech/top-20-ideas?sslmode=require"
-);
+const sql = neon(process.env.DRIZZLE_DATABASE_URL);
 export const db = drizzle(sql, { schema });
